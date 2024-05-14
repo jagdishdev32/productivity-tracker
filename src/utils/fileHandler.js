a390624452e38c049cb43fc7bd22539251fa39c3
jatin.besoya <jatin.besoya@originbluy.com>
2024-05-14 22:28:58 +0530

    Creating logs based on dates

--- src/utils/fileHandler.js
+++ src/utils/fileHandler.js
@@ -1,21 +1,31 @@
 const fs = require("fs");
 const { encryptData, decryptData } = require("./encryption");
+const { getCurrentDate } = require("./utils");
 
-const logFilePath = "logs/log.enc";
+const logDirectory = "logs/";
+
+const getLogFilePath = () => {
+  const currentDate = getCurrentDate();
+  return `${logDirectory}${currentDate}.log.enc`;
+};
 
 const saveLog = (log, passphrase) => {
+  const logFilePath = getLogFilePath();
   const data = JSON.stringify(log, null, 2);
   const encryptedData = encryptData(data, passphrase);
   fs.writeFileSync(logFilePath, encryptedData);
 };
 
 const loadLog = (passphrase) => {
+  const logFilePath = getLogFilePath();
   try {
     const encryptedData = fs.readFileSync(logFilePath, "utf-8");
     const data = decryptData(encryptedData, passphrase);
     return JSON.parse(data);
   } catch (err) {
-    console.warn("Could not decrypt log file. Starting with a new log.");
+    console.warn(
+      `Could not load log for ${getCurrentDate()}. Starting with a new log.`
+    );
     return [];
   }
 };
